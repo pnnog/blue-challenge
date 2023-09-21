2e195fe431d105daf7a67edc46c40502a18494d4
pnnog <pnnogdev@gmail.com>
2023-09-21 05:29:47 -0300

    feat: add logo link

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,10 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 import logo from 'assets/logo.png';
 
 import * as S from './styles';
 
+
 const Logo = () => {
+  const navigate = useNavigate();
+
   return(
-    <S.Wrapper>
+    <S.Wrapper onClick={()=> navigate('/')}>
       <img src={logo} alt="Logo da BLUE" />  
     </S.Wrapper>
   );
